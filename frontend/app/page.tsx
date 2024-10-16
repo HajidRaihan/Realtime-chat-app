36b7f0983af6d551821100c3b8164416e10cbd6b
Hajid Raihan <hajidraihan@gmail.com>
2024-10-16 17:01:03 +0800

    change folder name

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,17 +1,56 @@
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
-import { button as buttonStyles } from "@nextui-org/theme";
-
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Message from "@/components/Message";
+import MessageInput from "@/components/MessageInput";
+import BubbleChat from "@/components/BubbleChat";
 
 export default function Home() {
   return (
-    <div>
-      <p>asd</p>
-      <p>asdlakhjsikdlgklsjhfdiklgj</p>
+    <div className="flex relative w-full h-screen">
+      <div className="hidden md:flex bg-[#fff] border shadow-md w-80 h-full flex-col">
+        <div className="overflow-y-auto flex-1">
+          <Message
+            avatar="https://media.hitekno.com/thumbs/2022/07/19/93338-one-piece-monkey-d-luffy/730x480-img-93338-one-piece-monkey-d-luffy.jpg"
+            title="Luffy"
+            content="Oiii oiii"
+          />
+          <Message
+            avatar="https://media.hitekno.com/thumbs/2022/07/19/93338-one-piece-monkey-d-luffy/730x480-img-93338-one-piece-monkey-d-luffy.jpg"
+            title="Luffy"
+            content="Oiii oiii"
+          />
+          <Message
+            avatar="https://media.hitekno.com/thumbs/2022/07/19/93338-one-piece-monkey-d-luffy/730x480-img-93338-one-piece-monkey-d-luffy.jpg"
+            title="Luffy"
+            content="Oiii oiii"
+          />
+        </div>
+      </div>
+
+      {/* Konten chat */}
+      <div className="flex-1 relative flex flex-col ">
+        <div className="p-5 flex flex-col gap-3">
+          <BubbleChat avatar="#" message="hai" isUserMessage={false} />
+          <BubbleChat avatar="#" message="hai" isUserMessage={true} />
+          <BubbleChat
+            message="Hello! How are you?"
+            avatar="https://media.hitekno.com/thumbs/2022/07/19/93338-one-piece-monkey-d-luffy/730x480-img-93338-one-piece-monkey-d-luffy.jpg"
+          />
+
+          <BubbleChat
+            message="I'm good, thanks!"
+            avatar="https://user-avatar-url.com/avatar.jpg"
+            isUserMessage={true}
+          />
+          <BubbleChat
+            message="I'm good, thanks!"
+            avatar="https://user-avatar-url.com/avatar.jpg"
+            isUserMessage={true}
+          />
+        </div>
+        <div className="flex-1 overflow-auto p-4">{/* Chat messages section */}</div>
+        <MessageInput />
+      </div>
     </div>
   );
 }
--- frontend/components/SidebarItem.tsx
+++ frontend/components/SidebarItem.tsx
@@ -1,13 +1,29 @@
 import { IconType } from "react-icons";
+import { Button } from "./ui/button";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
 
 interface SidebarItemProps {
   icon: IconType;
   label: string;
   isActive?: boolean;
   href: string;
+  className?: string;
 }
-const SidebarItem: React.FC<SidebarItemProps> = ({ icon: Icon, label, isActive, href }) => {
-  return <div></div>;
+const SidebarItem: React.FC<SidebarItemProps> = ({
+  icon: Icon,
+  label,
+  isActive,
+  href,
+  className,
+}) => {
+  return (
+    <Link href={href}>
+      <Button size={"sm"} className={cn("bg-white", isActive && "bg-mainAccent")}>
+        <Icon size={20} />
+      </Button>
+    </Link>
+  );
 };
 
 export default SidebarItem;
